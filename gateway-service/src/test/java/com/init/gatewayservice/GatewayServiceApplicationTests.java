213f6fa9d04640532d1242d2ec8b02700fe8aa02
Leonel Sandoval <sandovalleonel16@gmail.com>
2022-09-05 16:46:02 -0500

    gateway para ingresa a los microservicios atraves de un solo puerto

---
+++ gateway-service/src/test/java/com/init/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.init.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
